d5c9c4c13e824b8f264abb054f3b4a1c7786dee6
Syed Mir Habib <mirakik.habib@gmail.com>
2023-04-09 21:54:55 +0600

    spelling correction: cusin to cousin

--- src/Components/Aunty/Aunty.jsx
+++ src/Components/Aunty/Aunty.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Cusin from '../Cusin/Cusin';
+import Cousin from '../Cousin/Cousin';
 
 const Aunty = ({ring}) => {
     return (
         <div>
             <h2>Aunty</h2>
             <section className='flex'>
-                <Cusin>Abir</Cusin>
-                <Cusin hasFriend={true} ring={ring}>Nibir</Cusin>
+                <Cousin>Abir</Cousin>
+                <Cousin hasFriend={true} ring={ring}>Nibir</Cousin>
             </section>
         </div>
     );
--- src/Components/Uncle/Uncle.jsx
+++ src/Components/Uncle/Uncle.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import Cusin from '../Cusin/Cusin';
+import Cousin from '../Cousin/Cousin';
 import { MoneyContext } from '../Grandpa/Grandpa';
 
 const Uncle = () => {
@@ -10,8 +10,8 @@ const Uncle = () => {
             <p><small>grandpa money: {money}</small></p>
             <button onClick={() => setMoney(money + 1000)}>Send 1000tk</button>
             <section className='flex'>
-                <Cusin>Nabil</Cusin>
-                <Cusin>Nabila</Cusin>
+                <Cousin>Nabil</Cousin>
+                <Cousin>Nabila</Cousin>
             </section>
         </div>
     );
